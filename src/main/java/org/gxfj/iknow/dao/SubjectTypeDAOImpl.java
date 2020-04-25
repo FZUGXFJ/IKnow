e264d970aa54beb81d1fc235c74d6bd12f62d701
QMBX <30679822+QMBX@users.noreply.github.com>
2020-04-25 20:42:55 +0800

    添加MajorTypeDAO和SubjectTypeDAO的lst方法及其实现

--- src/main/java/org/gxfj/iknow/dao/SubjectTypeDAOImpl.java
+++ src/main/java/org/gxfj/iknow/dao/SubjectTypeDAOImpl.java
@@ -1,11 +1,17 @@
 package org.gxfj.iknow.dao;
 
+import org.gxfj.iknow.pojo.Categoriestype;
 import org.gxfj.iknow.pojo.Subjecttype;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Repository("subjectTypeDAO")
 public class SubjectTypeDAOImpl implements SubjectTypeDAO{
@@ -37,5 +43,12 @@ public class SubjectTypeDAOImpl implements SubjectTypeDAO{
     @Override
     public void update(Subjecttype bean) {
         getHibernateTemplate().update(bean);
+    }
+
+    @Override
+    public List<Subjecttype> list(Integer categoryTypeID) {
+        String hql = "FROM Subjecttype as s WHERE s.categoriestypeByCategoryId.id = ?";
+        return (List<Subjecttype>) getHibernateTemplate().find(hql, categoryTypeID);
+
     }
 }
