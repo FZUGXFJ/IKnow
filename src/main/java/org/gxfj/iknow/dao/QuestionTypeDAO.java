0a2b627441868499adf561766866b0aba17683d6
QMBX <30679822+QMBX@users.noreply.github.com>
2020-04-25 14:41:39 +0800

    添加根据门类、专业类、专业3个id查询对应的questiontype

--- src/main/java/org/gxfj/iknow/dao/QuestionTypeDAO.java
+++ src/main/java/org/gxfj/iknow/dao/QuestionTypeDAO.java
@@ -2,10 +2,26 @@ package org.gxfj.iknow.dao;
 
 import org.gxfj.iknow.pojo.Questiontype;
 
+/**
+ * @author qmbx
+ */
 public interface QuestionTypeDAO {
+    /**
+     * 向数据库中添加问题类别
+     * @param bean 添加的类别
+     */
     public void add(Questiontype bean);
 
     public Questiontype get(Integer id);
+
+    /**
+     * 根据问题的categoryTypeID、subjectTypeID、majorTypeID查询对应Questiontype对象
+     * @param categoryTypeID 门类ID
+     * @param subjectTypeID 专业类ID
+     * @param majorTypeID 专业ID
+     * @return 没有返回null
+     */
+    public Questiontype get(Integer categoryTypeID, Integer subjectTypeID, Integer majorTypeID);
 
     public void update(Questiontype bean);
 }
