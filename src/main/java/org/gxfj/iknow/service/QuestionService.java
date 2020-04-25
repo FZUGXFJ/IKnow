5c1ce3316aa0d59f730231d4e5179d80581cab59
QMBX <30679822+QMBX@users.noreply.github.com>
2020-04-25 15:14:37 +0800

    添加postQuestion方法及其实现

--- src/main/java/org/gxfj/iknow/service/QuestionService.java
+++ src/main/java/org/gxfj/iknow/service/QuestionService.java
@@ -1,11 +1,14 @@
 package org.gxfj.iknow.service;
 
 
+import org.gxfj.iknow.pojo.Question;
+import org.gxfj.iknow.pojo.User;
 
 /**
  * @author qmbx
  */
 public interface QuestionService {
-
+    public Question postQuestion(User user, String title, String context, Integer categoryType, Integer subjectType
+        , Integer majorType, Byte isAnonymous);
 
 }
--- src/main/java/org/gxfj/iknow/service/QuestionServiceImpl.java
+++ src/main/java/org/gxfj/iknow/service/QuestionServiceImpl.java
@@ -1,7 +1,12 @@
 package org.gxfj.iknow.service;
 
+import org.gxfj.iknow.dao.QuestionDAO;
+import org.gxfj.iknow.dao.QuestionTypeDAO;
+import org.gxfj.iknow.pojo.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 
 
 /**
@@ -9,5 +14,38 @@ import org.springframework.stereotype.Service;
  */
 @Service("questionService")
 public class QuestionServiceImpl implements QuestionService{
+    @Autowired
+    QuestionDAO questionDAO;
+    @Autowired
+    QuestionTypeDAO questionTypeDAO;
 
+    @Override
+    public Question postQuestion(User user, String title, String context, Integer categoryType, Integer subjectType
+            , Integer majorType, Byte isAnonymous) {
+        Question question = new Question();
+        Questiontype questiontype = questionTypeDAO.get(categoryType,subjectType,majorType);
+        Questionstate questionstate = new Questionstate();
+        Questionscenario questionscenario = new Questionscenario();
+        Answer answer = new Answer();
+
+        //TODO 修正魔法值的存在
+        questionstate.setId(1);
+        questionscenario.setId(1);
+        answer.setId(0);
+
+        question.setUserByUserId(user);
+        question.setTitle(title);
+        question.setContent(context);
+        question.setQuestiontypeByTypeId(questiontype);
+        question.setQuestionstateByStateId(questionstate);
+        question.setQuestionscenarioByScenarioId(questionscenario);
+        question.setDate(new Date());
+        question.setIsDelete((byte)0);
+        question.setIsAnonymous(isAnonymous);
+
+
+        questionDAO.add(question);
+
+        return null;
+    }
 }
