c4f98517dcb5fb4bbb3d3a386bb660502efee910
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-04-27 12:04:07 +0800

    新增功能
    新增获取回答评论数功能

--- src/main/java/org/gxfj/iknow/dao/CommentDAOimpl.java
+++ src/main/java/org/gxfj/iknow/dao/CommentDAOimpl.java
@@ -1,11 +1,19 @@
 package org.gxfj.iknow.dao;
 
+import org.gxfj.iknow.pojo.Answer;
 import org.gxfj.iknow.pojo.Comment;
+import org.gxfj.iknow.pojo.Level;
+import org.gxfj.iknow.pojo.User;
+import org.hibernate.HibernateException;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateCallback;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository("commentDAO")
 public class CommentDAOimpl implements CommentDAO{
@@ -38,5 +46,22 @@ public class CommentDAOimpl implements CommentDAO{
     @Override
     public void update(Comment bean) {
         getHibernateTemplate().update(bean);
+    }
+
+    @Override
+    public Integer getCount(Integer answerId){
+        List<Comment> list=getHibernateTemplate().execute(new HibernateCallback<List<Comment>>() {
+            @Override
+            public List<Comment> doInHibernate(Session session) throws HibernateException {
+                SQLQuery sqlQuery=session.createSQLQuery("select * from comment where " +
+                        "answerID = " +answerId).addEntity(Comment.class);
+                return sqlQuery.list();
+            }
+        });
+        if (list.isEmpty()) {
+            return 0;
+        } else {
+            return list.size();
+        }
     }
 }
