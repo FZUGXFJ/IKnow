435e8395606efe6a3e8f4b9e49f76bb716ff7648
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-04-28 14:44:40 +0800

    CommentServiceImpl添加了发布评论的函数

--- src/main/java/org/gxfj/iknow/service/CommentServiceImpl.java
+++ src/main/java/org/gxfj/iknow/service/CommentServiceImpl.java
@@ -13,8 +13,20 @@ import java.util.*;
 public class CommentServiceImpl implements CommentService {
     @Autowired
     CommentDAO commentDAO;
+    @Autowired
+    AnswerDAO answerDAO;
 
     @Override
-    public void postComment(User user, Integer answerID, String content){
+    public void postComment(User user, Integer answerId, String content){
+        Comment comment = new Comment();
+
+        comment.setUserByUserId(user);
+        comment.setContent(content);
+        comment.setAnswerByAnswerId(answerDAO.get(answerId));
+        comment.setDate(new Date());
+        comment.setIsDelete((byte)0);
+        comment.setCount(0);
+
+        commentDAO.add(comment);
     }
 }
