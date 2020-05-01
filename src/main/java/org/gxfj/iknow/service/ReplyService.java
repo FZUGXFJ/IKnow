a6b85dd2b53cc1c6ec482bcce78b7bedc780d23c
fzu-hhj <60642165+fzu-hhj@users.noreply.github.com>
2020-05-01 13:52:43 +0800

    添加了replyService的函数接口

--- src/main/java/org/gxfj/iknow/service/ReplyService.java
+++ src/main/java/org/gxfj/iknow/service/ReplyService.java
@@ -5,7 +5,7 @@ import org.gxfj.iknow.pojo.User;
 import java.util.Map;
 
 /**
- * @author erniumo
+ * @author erniumo ， hhj
  * @author
  */
 public interface ReplyService {
@@ -16,4 +16,11 @@ public interface ReplyService {
      * content 评论的内容
      */
     public void postReply(Integer commentID, String content, Integer replyTarget, User user);
+
+    /**
+     * 查看全部回复
+     * @param commentId 评论
+     * @return json格式的评论信息和所有回复信息
+     */
+    public Map<String , Object> showAllReplys(Integer commentId);
 }
