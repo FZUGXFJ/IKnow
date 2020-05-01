533264c12c8a9b0e2cb78bbaf85b8a5b992e1c48
erniumo <936055622@qq.com>
2020-05-01 12:26:01 +0800

    postReply完成

--- src/main/java/org/gxfj/iknow/service/ReplyService.java
+++ src/main/java/org/gxfj/iknow/service/ReplyService.java
@@ -1,7 +1,19 @@
 package org.gxfj.iknow.service;
+
+import org.gxfj.iknow.pojo.User;
+
+import java.util.Map;
+
 /**
  * @author erniumo
  * @author
  */
 public interface ReplyService {
+    /**
+     * 提交回复
+     * user 提交用户
+     * commentId 评论的id
+     * content 评论的内容
+     */
+    public void postReply(Integer commentID, String content, Integer replyTarget, User user);
 }
--- src/main/java/org/gxfj/iknow/service/ReplyServiceImpl.java
+++ src/main/java/org/gxfj/iknow/service/ReplyServiceImpl.java
@@ -1,4 +1,37 @@
 package org.gxfj.iknow.service;
 
+import org.gxfj.iknow.dao.CommentDAO;
+import org.gxfj.iknow.dao.ReplyDAO;
+import org.gxfj.iknow.dao.UserDAO;
+import org.gxfj.iknow.pojo.Comment;
+import org.gxfj.iknow.pojo.Reply;
+import org.gxfj.iknow.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.sql.Timestamp;
+import java.util.*;
+
 public class ReplyServiceImpl implements ReplyService {
+    @Autowired
+    private UserDAO userDAO;
+    @Autowired
+    private CommentDAO commentDAO;
+    @Autowired
+    private ReplyDAO replyDAO;
+    @Override
+    public void postReply(Integer commentID, String content, Integer replyTarget, User user) {
+        User tu= userDAO.get(replyTarget);
+        Comment comment=commentDAO.get(commentID);
+
+        Reply reply=new Reply();
+        reply.setContent(content);
+        reply.setIsDelete((byte)0);
+        reply.setCount(0);
+        reply.setDate((Timestamp) new Date());
+        reply.setUserByTargetUserId(tu);
+        reply.setCommentByCommentId(comment);
+        reply.setUserByUserId(user);
+
+        replyDAO.add(reply);
+    }
 }
