549f337de2762a3d37b9f435a8ef6518a530f2f2
QMBX <30679822+QMBX@users.noreply.github.com>
2020-05-03 15:02:09 +0800

    添加查询记录的方法

--- src/main/java/org/gxfj/iknow/dao/ApprovalCommentDAO.java
+++ src/main/java/org/gxfj/iknow/dao/ApprovalCommentDAO.java
@@ -5,7 +5,21 @@ import org.gxfj.iknow.pojo.Approvalcomment;
 public interface ApprovalCommentDAO {
     public void add(Approvalcomment bean);
 
+    /**
+     * 根据评论点赞记录的id查询评论点赞记录
+     * @param id 评论点赞记录的id
+     * @return
+     */
     public Approvalcomment get(Integer id);
+
+
+    /**
+     * 根据用户id和评论id获得对应的点赞记录
+     * @param userId 用户id
+     * @param commentId 评论id
+     * @return 查询到的记录，如果没有，则为null
+     */
+    public Approvalcomment get(Integer userId, Integer commentId);
 
     public void update(Approvalcomment bean);
 
@@ -15,4 +29,6 @@ public interface ApprovalCommentDAO {
      * @return  评论赞同数
      */
     public Integer getCount(Integer commentId);
+
+
 }
