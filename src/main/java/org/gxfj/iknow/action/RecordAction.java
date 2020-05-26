af215b6b9734da9b497045564098887fa3b43bd8
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-05-26 14:43:11 +0800

    新建RecordAction

---
+++ src/main/java/org/gxfj/iknow/action/RecordAction.java
@@ -0,0 +1,23 @@
+package org.gxfj.iknow.action;
+
+import com.alibaba.fastjson.JSON;
+import com.opensymphony.xwork2.ActionContext;
+import org.gxfj.iknow.pojo.User;
+import org.gxfj.iknow.service.AnswerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author 爱学习的水先生
+ */
+
+
+@Controller
+public class RecordAction {
+}
