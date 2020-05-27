f665fd1d5269a1210b76633499bb1564ff01876c
fzu-hhj <60642165+fzu-hhj@users.noreply.github.com>
2020-05-27 15:48:53 +0800

    获得用户所有身份的方法

--- src/main/java/org/gxfj/iknow/dao/UserIdentityDAO.java
+++ src/main/java/org/gxfj/iknow/dao/UserIdentityDAO.java
@@ -2,6 +2,15 @@ package org.gxfj.iknow.dao;
 
 import org.gxfj.iknow.pojo.Useridentity;
 
+import java.util.List;
+
 public interface UserIdentityDAO extends BaseDAO<Useridentity>{
+
+    /**
+     * 获得用户的所有身份
+     * @param userId 用户id
+     * @return 用户身份列表，没有返回null
+     */
+    public List<Useridentity> listUserIdentitiesByUserId (Integer userId);
 
 }
