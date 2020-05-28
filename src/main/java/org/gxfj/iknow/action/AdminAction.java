7ef55c8c94b0746d8a26bee72f175ad0c4ea5414
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-05-28 19:15:53 +0800

    新增管理员退出登录

--- src/main/java/org/gxfj/iknow/action/AdminAction.java
+++ src/main/java/org/gxfj/iknow/action/AdminAction.java
@@ -1,4 +1,36 @@
 package org.gxfj.iknow.action;
 
+import com.alibaba.fastjson.JSON;
+import com.opensymphony.xwork2.ActionContext;
+import org.apache.struts2.ServletActionContext;
+import org.gxfj.iknow.pojo.User;
+import org.gxfj.iknow.service.UserService;
+import org.gxfj.iknow.util.MailUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author hhj
+ */
+@Controller
 public class AdminAction {
+    private InputStream inputStream;
+    private final String SUCCESS = "success";
+
+    public String logout(){
+        Map<String,Object> session = ActionContext.getContext().getSession();
+        if(!session.isEmpty()){
+            session.clear();
+        }
+        inputStream = new ByteArrayInputStream("{\"response\":0}".getBytes(StandardCharsets.UTF_8));
+        return  SUCCESS;
+    }
 }
