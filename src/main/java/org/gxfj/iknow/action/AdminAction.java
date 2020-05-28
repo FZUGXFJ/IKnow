9181f30c4f0555875e939212472b0f42e1756a17
fzu-hhj <60642165+fzu-hhj@users.noreply.github.com>
2020-05-28 19:49:13 +0800

    管理员登录的后端实现

--- src/main/java/org/gxfj/iknow/action/AdminAction.java
+++ src/main/java/org/gxfj/iknow/action/AdminAction.java
@@ -3,7 +3,9 @@ package org.gxfj.iknow.action;
 import com.alibaba.fastjson.JSON;
 import com.opensymphony.xwork2.ActionContext;
 import org.apache.struts2.ServletActionContext;
+import org.gxfj.iknow.pojo.Admin;
 import org.gxfj.iknow.pojo.User;
+import org.gxfj.iknow.service.AdminService;
 import org.gxfj.iknow.service.UserService;
 import org.gxfj.iknow.util.MailUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,8 +24,15 @@ import java.util.Map;
  */
 @Controller
 public class AdminAction {
+    private Integer accountNum;
+    private String password;
+
+    @Autowired
+    private AdminService adminService;
+
     private InputStream inputStream;
     private final String SUCCESS = "success";
+    private static Integer HASH_MAP_NUM = 20;
 
     public String logout(){
         Map<String,Object> session = ActionContext.getContext().getSession();
@@ -32,5 +41,21 @@ public class AdminAction {
         }
         inputStream = new ByteArrayInputStream("{\"response\":0}".getBytes(StandardCharsets.UTF_8));
         return  SUCCESS;
+    }
+
+    public String login(){
+        Map resultMap = new HashMap(HASH_MAP_NUM);
+        Admin adminInf = new Admin();
+        adminInf.setAccount(accountNum);
+        adminInf.setPasswd(password);
+        Admin admin = adminService.login(adminInf);
+        if(admin == null){
+            resultMap.put("resultCode" , 1);
+        }
+        else{
+            resultMap.put("resultCode" , 0);
+        }
+        inputStream = new ByteArrayInputStream(JSON.toJSONString(resultMap).getBytes(StandardCharsets.UTF_8));
+        return SUCCESS;
     }
 }
--- src/main/java/org/gxfj/iknow/service/AdminService.java
+++ src/main/java/org/gxfj/iknow/service/AdminService.java
@@ -1,4 +1,13 @@
 package org.gxfj.iknow.service;
 
+import org.gxfj.iknow.pojo.Admin;
+
 public interface AdminService {
+
+    /**
+     * 管理员登录
+     * @param adminInf 输入的管理员信息
+     * @return 管理员
+     */
+    public Admin login(Admin adminInf);
 }
