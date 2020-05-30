6f8bc13157b8abfff5687c4af043ea9559248913
erniumo <936055622@qq.com>
2020-05-30 18:28:10 +0800

    分类前三个

--- src/main/java/org/gxfj/iknow/service/PartitionService.java
+++ src/main/java/org/gxfj/iknow/service/PartitionService.java
@@ -1,4 +1,26 @@
 package org.gxfj.iknow.service;
 
+import java.util.Map;
+
 public interface PartitionService {
+
+    /**
+     * 获得门类json
+     * @return 数据
+     */
+    public Map<String,Object> getCategories();
+
+    /**
+     * 获得门类下所有学科json
+     * @param cid 门类id
+     * @return 数据
+     */
+    public Map<String,Object> getSubjects(Integer cid);
+
+    /**
+     * 获得学科下所有专业json
+     * @param sid 学科id
+     * @return 数据
+     */
+    public Map<String,Object> getMajors(Integer sid);
 }
--- src/main/java/org/gxfj/iknow/service/PartitionServiceImpl.java
+++ src/main/java/org/gxfj/iknow/service/PartitionServiceImpl.java
@@ -1,7 +1,80 @@
 package org.gxfj.iknow.service;
 
+import org.gxfj.iknow.dao.CategoriesTypeDAO;
+import org.gxfj.iknow.dao.MajorTypeDAO;
+import org.gxfj.iknow.dao.SubjectTypeDAO;
+import org.gxfj.iknow.pojo.Categoriestype;
+import org.gxfj.iknow.pojo.Major;
+import org.gxfj.iknow.pojo.Majortype;
+import org.gxfj.iknow.pojo.Subjecttype;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service("partitionService")
 public class PartitionServiceImpl implements PartitionService{
+    @Autowired
+    CategoriesTypeDAO categoriesTypeDAO;
+    @Autowired
+    SubjectTypeDAO subjectTypeDAO;
+    @Autowired
+    MajorTypeDAO majorTypeDAO;
+    @Override
+    public Map<String, Object> getCategories() {
+        Map<String,Object> category;
+        Map<String,Object> result=new HashMap<>(2);
+        List<Map<String,Object>> categories=new ArrayList<>();
+
+        List<Categoriestype> categoriestypes= categoriesTypeDAO.list();
+        for(Categoriestype categoriestype:categoriestypes){
+            category=new HashMap<>(2);
+            category.put("id",categoriestype.getId());
+            category.put("name",categoriestype.getName());
+
+            categories.add(category);
+        }
+        result.put("categories",categories);
+        return result;
+    }
+
+    @Override
+    public Map<String, Object> getSubjects(Integer cid) {
+        Map<String,Object> subject;
+        Map<String,Object> result=new HashMap<>(2);
+        List<Map<String,Object>> subjects=new ArrayList<>();
+
+        List<Subjecttype> subjecttypes=subjectTypeDAO.list(cid);
+        for (Subjecttype subjecttype:subjecttypes){
+            subject=new HashMap<>(2);
+            subject.put("id",subjecttype.getId());
+            subject.put("name",subjecttype.getName());
+
+            subjects.add(subject);
+        }
+        result.put("subjects",subjects);
+        return result;
+    }
+
+    @Override
+    public Map<String, Object> getMajors(Integer sid) {
+        Map<String,Object> major;
+        Map<String,Object> result=new HashMap<>(2);
+        List<Map<String,Object>> majors=new ArrayList<>();
+
+        List<Majortype> majortypes=majorTypeDAO.list(sid);
+        for (Majortype majortype:majortypes){
+            major=new HashMap<>(2);
+            major.put("id",majortype.getId());
+            major.put("name",majortype.getName());
+
+            majors.add(major);
+        }
+
+        result.put("majors",majors);
+        return result;
+    }
 }
