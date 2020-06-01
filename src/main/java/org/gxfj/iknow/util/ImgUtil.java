2979319fad7e2c03c30b500e57fb7a2306a8b943
QMBX <30679822+QMBX@users.noreply.github.com>
2020-06-01 15:48:09 +0800

    添加图片工具类
    添加changeAvatar方法

---
+++ src/main/java/org/gxfj/iknow/util/ImgUtil.java
@@ -0,0 +1,20 @@
+package org.gxfj.iknow.util;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author QMBX
+ */
+@Component
+public class ImgUtil {
+    /**
+     * 将数据库中的头像转化为html代码
+     * @param fileName 头像的名称(带后缀)
+     * @return 头像对应的html代码
+     */
+    public static String changeAvatar(String fileName) {
+        return "<img src='../head/" + fileName + "' width='100%' height='100%' style='border-radius: 100%' alt=''>";
+    }
+
+
+}
