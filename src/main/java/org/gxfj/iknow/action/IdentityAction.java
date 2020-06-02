a967760c5fc7a3a301f946a39fd35e1173b7205e
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-06-02 22:57:25 +0800

    新增IdentityAction接口

---
+++ src/main/java/org/gxfj/iknow/action/IdentityAction.java
@@ -0,0 +1,20 @@
+package org.gxfj.iknow.action;
+
+import com.alibaba.fastjson.JSON;
+import org.gxfj.iknow.service.IdentityService;
+import org.gxfj.iknow.util.ConstantUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+/**
+ * @author 爱学习的水先生
+ */
+
+@Controller
+public class IdentityAction {
+}
