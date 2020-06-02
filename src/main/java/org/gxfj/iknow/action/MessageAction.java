1c5a57c371357f64f5756ef4caebda2f1c2fd88c
erniumo <936055622@qq.com>
2020-06-02 22:46:49 +0800

    消息处理Action

--- src/main/java/org/gxfj/iknow/action/MessageAction.java
+++ src/main/java/org/gxfj/iknow/action/MessageAction.java
@@ -1,8 +1,17 @@
 package org.gxfj.iknow.action;
 
+import com.alibaba.fastjson.JSON;
+import com.opensymphony.xwork2.ActionContext;
+import org.gxfj.iknow.pojo.User;
+import org.gxfj.iknow.service.MessageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.gxfj.iknow.util.ConstantUtil.*;
 
@@ -12,6 +21,9 @@ import static org.gxfj.iknow.util.ConstantUtil.*;
 @Controller
 public class MessageAction {
 
+    @Autowired
+    MessageService messageService;
+
     private Integer messageId;
     private InputStream inputStream;
 
@@ -20,6 +32,17 @@ public class MessageAction {
      * @return 前端
      */
     public String getMessage(){
+        Map<String, Object> session = ActionContext.getContext().getSession();
+        Map<String, Object> response;
+        User user = (User)session.get(SESSION_USER);
+        if(user == null){
+            response=messageService.messageInf(null);
+        }
+        else {
+            response=messageService.messageInf(user);
+        }
+
+        inputStream = new ByteArrayInputStream(JSON.toJSONString(response).getBytes(StandardCharsets.UTF_8));
         return RETURN_STRING;
     }
 
@@ -28,6 +51,17 @@ public class MessageAction {
      * @return 前端
      */
     public String readMessage(){
+        Map<String, Object> session = ActionContext.getContext().getSession();
+        Map<String, Object> response;
+        User user = (User)session.get(SESSION_USER);
+        if(user == null){
+            response=messageService.readMessage(null,messageId);
+        }
+        else {
+            response=messageService.readMessage(user,messageId);
+        }
+
+        inputStream = new ByteArrayInputStream(JSON.toJSONString(response).getBytes(StandardCharsets.UTF_8));
         return RETURN_STRING;
     }
     public InputStream getInputStream() {
