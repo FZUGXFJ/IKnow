d05cc4754bf2adab51b2ceaaec50783e137468f5
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-06-02 22:57:45 +0800

    新增IdentityService接口以及实现类

---
+++ src/main/java/org/gxfj/iknow/service/IdentityService.java
@@ -0,0 +1,11 @@
+package org.gxfj.iknow.service;
+
+import org.gxfj.iknow.pojo.Useridentity;
+
+import java.util.Map;
+
+/**
+ * @author 爱学习的水先生
+ */
+public interface IdentityService {
+}
---
+++ src/main/java/org/gxfj/iknow/service/IdentityServiceImpl.java
@@ -0,0 +1,27 @@
+package org.gxfj.iknow.service;
+
+import com.opensymphony.xwork2.ActionContext;
+import org.gxfj.iknow.dao.*;
+import org.gxfj.iknow.pojo.*;
+import org.gxfj.iknow.util.ConstantUtil;
+import org.gxfj.iknow.util.SecurityUtil;
+import static org.gxfj.iknow.util.ServiceConstantUtil.*;
+
+import org.gxfj.iknow.util.Time;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author 爱学习的水先生
+ */
+@Service("identityService")
+public class IdentityServiceImpl implements IdentityService{
+}
