956c35efe0fce5eff43b6a010156bf41eee3b8dc
爱学习的水先生 <60643156+shuiYe-704265085@users.noreply.github.com>
2020-06-03 09:33:12 +0800

    SchoolDAO新增获得包含关键字的学校列表接口并实现

--- src/main/java/org/gxfj/iknow/dao/SchoolDAO.java
+++ src/main/java/org/gxfj/iknow/dao/SchoolDAO.java
@@ -1,7 +1,15 @@
 package org.gxfj.iknow.dao;
 
+import org.gxfj.iknow.pojo.Reply;
 import org.gxfj.iknow.pojo.School;
 
-public interface SchoolDAO extends BaseDAO<School>{
+import java.util.List;
 
+public interface SchoolDAO extends BaseDAO<School>{
+    /**
+     * 获得包含关键字的学校列表
+     * @param keyword 关键字
+     * @return  学校列表
+     */
+    List<School> listKewordSchool(String keyword);
 }
